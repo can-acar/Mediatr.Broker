8751a9db22e90c0072bf5a085338395303935143
Can Acar <can.acar2@ogr.sakarya.edu.tr>
2024-05-19 01:02:53 +0300

    feature update

--- Mediatr.Broker/MediatorClient.cs
+++ Mediatr.Broker/MediatorClient.cs
@@ -10,13 +10,11 @@ namespace Mediatr.Broker;
 public class ReceiveMessage
 {
     public required string Type { get; set; }
-
     public required string Name { get; set; }
-
     public required object Payload { get; set; }
 }
 
-public class MediatorClient(int port, string host, string name) : IMediatorClient
+public class MediatorClient(int port, string host, string name):IMediatorClient
 {
     private readonly int _port = port;
     private readonly UdpClient _client = new();
@@ -25,17 +23,14 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
     private readonly ConcurrentDictionary<Type, Func<object, CancellationToken, Task<object>>> _handlers = new();
     private ConcurrentDictionary<Type, Func<object, CancellationToken, Task>> _notificationHandlers = new();
 
-
     public async Task Register()
     {
         // aggregate all registered handlers and notification handlers from the mediator and send them to the mediator broker
         // _client.Client.Bind(new IPEndPoint(IPAddress.Any, port));
-
         await _client.Client.ConnectAsync(new IPEndPoint(IPAddress.Loopback, _port));
 
-
         // check if the client is connected to the server
-        if (_client.Client.Connected)
+        if(_client.Client.Connected)
         {
             _logger.LogInformation("Connected to the server");
             _logger.LogWarning("Connected to the server");
@@ -47,19 +42,16 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
         }
 
         var handlers = GetHandlers();
-
         var notificationHandlers = GetNotificationHandlers();
-
-        foreach (var handler in handlers)
+        foreach(var handler in handlers)
         {
             await SendHandler(handler);
 
             //var method = new  Func<object, CancellationToken, Task<object>> {(request, cancellationToken) => ExecuteHandlerAsync(handler, (IRequest<object>) request, cancellationToken)};
-
-            _handlers.TryAdd(handler, (request, cancellationToken) => ExecuteHandlerAsync(handler, (IRequest<object>) request, cancellationToken));
+            _handlers.TryAdd(handler, (request, cancellationToken) => ExecuteHandlerAsync(handler, (IRequest<object>)request, cancellationToken));
         }
 
-        foreach (var notificationHandler in notificationHandlers)
+        foreach(var notificationHandler in notificationHandlers)
         {
             await SendNotificationHandler(notificationHandler);
         }
@@ -68,69 +60,51 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
     private Task<object> ExecuteHandlerAsync(Type handler, IRequest<object> request, CancellationToken cancellationToken)
     {
         var handlerInstance = Activator.CreateInstance(handler);
-
         var method = handler.GetMethod("HandleAsync");
-
         var result = method!.Invoke(handlerInstance, new object[] {request, cancellationToken});
-
-        return (Task<object>) result!;
+        return (Task<object>)result!;
     }
 
     public async Task Run()
     {
         // wait for the mediator broker to send a message back and then handle it accordingly 
-        while (!_cts.Token.IsCancellationRequested)
+        while(!_cts.Token.IsCancellationRequested)
         {
             var result = await _client.ReceiveAsync(cancellationToken: _cts.Token);
-
             var message = Encoding.UTF8.GetString(result.Buffer);
-
             var data = JsonSerializer.Deserialize<ReceiveMessage>(message);
-
             Console.WriteLine($"Receive message: {data!.Name}");
-
             var handlerType = typeof(IHandler<,>);
             var notificationHandlerType = typeof(INotificationHandler<>);
 
             // cast the data payload to a NodeHandlerType or NodeNotificationHandlerType
-
-
-            if (data.Type == "Handler")
+            if(data.Type == "Handler")
             {
                 var item = JsonSerializer.Deserialize<NodeHandlerType>(JsonSerializer.Serialize(data.Payload));
                 var requestType = Assembly.Load(item.RequestTypeAssemblyName).GetType(item.RequestType);
                 var responseType = Assembly.Load(item.ResponseTypeAssemblyName).GetType(item.ResponseType);
-
                 var handler = typeof(MediatorClient).GetMethod("Handler")!.MakeGenericMethod(requestType, responseType);
-
-                await (Task) handler.Invoke(this, new object[] {data.Payload});
+                await (Task)handler.Invoke(this, new object[] {data.Payload});
             }
-            else if (data.Type == "NotificationHandler")
+            else if(data.Type == "NotificationHandler")
             {
                 var item = JsonSerializer.Deserialize<NodeNotificationHandlerType>(JsonSerializer.Serialize(data.Payload));
-
                 var notificationType = Assembly.Load(item.NotificationTypeAssemblyName).GetType(item.NotificationType);
-
                 var handler = typeof(MediatorClient).GetMethod("Notify")!.MakeGenericMethod(notificationType);
-
-                await (Task) handler.Invoke(this, new object[] {data.Payload});
+                await (Task)handler.Invoke(this, new object[] {data.Payload});
             }
 
-
             // deserialize the message into a payload
-
 
             // get the message and handle it accordingly
         }
     }
 
-
     private IEnumerable<Type> GetHandlers()
     {
         var handlerType = typeof(IHandler<,>);
         var handlerTypes = Assembly.GetEntryAssembly()!.GetTypes()
-            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerType));
-
+                                   .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == handlerType));
         return handlerTypes;
     }
 
@@ -138,8 +112,7 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
     {
         var notificationHandlerType = typeof(INotificationHandler<>);
         var notificationHandlerTypes = Assembly.GetEntryAssembly()!.GetTypes()
-            .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == notificationHandlerType));
-
+                                               .Where(t => t.GetInterfaces().Any(i => i.IsGenericType && i.GetGenericTypeDefinition() == notificationHandlerType));
         return notificationHandlerTypes;
     }
 
@@ -148,13 +121,10 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
         var handlerName = handler.Name;
         var requestType = handler.GetInterfaces().First().GetGenericArguments().First();
         var responseType = handler.GetInterfaces().First().GetGenericArguments().Last();
-
         var requestTypeName = requestType.Name;
         var responseTypeName = responseType.Name;
-
         var requestTypeAssemblyName = requestType.Assembly.GetName().Name;
         var responseTypeAssemblyName = responseType.Assembly.GetName().Name;
-
         var message = new
         {
             Id = Guid.NewGuid(),
@@ -170,24 +140,17 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
             ClientName = name,
             IpAddress = Dns.GetHostAddresses(Dns.GetHostName()).First()
         };
-
         var json = JsonSerializer.Serialize(message);
-
         var bytes = Encoding.UTF8.GetBytes(json);
-
         await _client.SendAsync(bytes, bytes.Length, host, _port);
     }
-
 
     private async Task SendNotificationHandler(Type notificationHandler)
     {
         var handlerName = notificationHandler.Name;
         var notificationType = notificationHandler.GetInterfaces().First().GetGenericArguments().First();
-
         var notificationTypeName = notificationType.Name;
-
         var notificationTypeAssemblyName = notificationType.Assembly.GetName().Name;
-
         var message = new
         {
             Id = Guid.NewGuid(),
@@ -200,10 +163,8 @@ public class MediatorClient(int port, string host, string name) : IMediatorClien
             ClientName = name,
             IpAddress = Dns.GetHostAddresses(Dns.GetHostName()).First()
         };
-
         var json = JsonSerializer.Serialize(message);
         var bytes = Encoding.UTF8.GetBytes(json);
-
         await _client.SendAsync(bytes, bytes.Length, host, _port);
     }
 }
--- Mediatr.Broker/MediatorServer.cs
+++ Mediatr.Broker/MediatorServer.cs
@@ -50,17 +50,10 @@ public class MediatorServer : IMediatorServer
     private IPEndPoint _endPoint;
     private readonly string _name;
     private Socket _socket;
-
     private readonly ILogger _logger;
-
     private readonly ConcurrentDictionary<IPEndPoint, string> _connectedClients = new();
-
-
     private ConcurrentQueue<string> _nodeQueue = new();
-
     private readonly Node _node = new();
-
-
     private CancellationTokenSource _cts;
 
     public MediatorServer(int port, string host, string name)
@@ -76,81 +69,62 @@ public class MediatorServer : IMediatorServer
     public async Task StartAsync(CancellationToken cancellationToken = default)
     {
         Console.WriteLine("Mediator Broker is running...");
-
         try
         {
             _socket = new Socket(_endPoint.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
             _socket.Bind(_endPoint);
 
-
             //var processMessagesTask = Task.Run(ProcessMessagesAsync, _cts.Token);
-
-
-            while (true)
+            while(true)
             {
-                byte[] data = new byte[1024];
-
+                var data = new byte[1024];
                 var brokerName = Encoding.UTF8.GetBytes(_name);
 
                 //await _udpClient.SendAsync(brokerName, brokerName.Length);
-
                 var available = _socket.Available;
+                if(_socket.Available <= 0) continue;
+                Console.WriteLine("Mediator Broker is receiving...");
+                var size = await _socket.ReceiveAsync(data, cancellationToken);
 
-                if (_socket.Available > 0)
+                // if (_socket.Connected == false)
+                // {
+                //     _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                //     _socket.Bind(_endPoint);
+                // }
+                if(!_socket.Connected || _socket.Available <= 0) continue;
+                var payload = Encoding.UTF8.GetString(data, 0, size);
+                var nodeHandlerType = JsonSerializer.Deserialize<NodeHandlerType>(payload);
+                var nodeNotificationHandlerType = JsonSerializer.Deserialize<NodeNotificationHandlerType>(payload);
+                if(nodeHandlerType != null)
                 {
-                    Console.WriteLine("Mediator Broker is receiving...");
-
-                    var size = await _socket.ReceiveAsync(data, cancellationToken);
-
-                    // if (_socket.Connected == false)
-                    // {
-                    //     _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-                    //     _socket.Bind(_endPoint);
-                    // }
-
-                    if (_socket.Connected && _socket.Available > 0)
+                    // add the handler to the node queue
+                    _node.Name = nodeHandlerType.Name;
+                    if (!_node.handlers.ContainsKey(Type.GetType(nodeHandlerType.RequestType)))
                     {
-                        var payload = Encoding.UTF8.GetString(data, 0, size);
-
-                        var nodeHandlerType = JsonSerializer.Deserialize<NodeHandlerType>(payload);
-
-                        var nodeNotificationHandlerType = JsonSerializer.Deserialize<NodeNotificationHandlerType>(payload);
-
-                        if (nodeHandlerType != null)
-                        {
-                            // add the handler to the node queue
-                            _node.Name = nodeHandlerType.Name;
-
-                            if (!_node.handlers.ContainsKey(Type.GetType(nodeHandlerType.RequestType)))
-                            {
-                                _node.handlers.TryAdd(Type.GetType(nodeHandlerType.RequestType), (request, cancellationToken) =>
-                                    ExecuteHandlerAsync(nodeHandlerType, request, cancellationToken));
-                            }
-
-                            //_nodes.TryAdd(node.Name, node);
-                            //_nodeQueue.Enqueue(node);
-                            _nodeQueue.Enqueue(_node.Name);
-                        }
-                        else if (nodeNotificationHandlerType != null)
-                        {
-                            // add the notification handler to the node queue
-
-                            _node.Name = nodeNotificationHandlerType.Name;
-
-                            if (!_node.notificationHandlers.ContainsKey(Type.GetType(nodeNotificationHandlerType.NotificationType)))
-                            {
-                                _node.notificationHandlers.TryAdd(Type.GetType(nodeNotificationHandlerType.NotificationType), (notification, cancellationToken) =>
-                                    ExecuteNotificationHandlerAsync(nodeNotificationHandlerType, notification, cancellationToken));
-                            }
-
-                            // _nodes.TryAdd(node.Name, node);
-                            _nodeQueue.Enqueue(_node.Name);
-                        }
+                        _node.handlers.TryAdd(Type.GetType(nodeHandlerType.RequestType), (request, cancellationToken) =>
+                                                  ExecuteHandlerAsync(nodeHandlerType, request, cancellationToken));
                     }
+
+                    //_nodes.TryAdd(node.Name, node);
+                    //_nodeQueue.Enqueue(node);
+                    _nodeQueue.Enqueue(_node.Name);
+                }
+                else if(nodeNotificationHandlerType != null)
+                {
+                    // add the notification handler to the node queue
+                    _node.Name = nodeNotificationHandlerType.Name;
+                    if(!_node.notificationHandlers.ContainsKey(Type.GetType(nodeNotificationHandlerType.NotificationType)))
+                    {
+                        _node.notificationHandlers.TryAdd(Type.GetType(nodeNotificationHandlerType.NotificationType), (notification, cancellationToken) =>
+                                                              ExecuteNotificationHandlerAsync(nodeNotificationHandlerType, notification, cancellationToken));
+                    }
+
+                    // _nodes.TryAdd(node.Name, node);
+                    _nodeQueue.Enqueue(_node.Name);
                 }
             }
         }
-        catch (Exception e)
+        catch(Exception e)
         {
             Console.WriteLine(e);
             throw;
@@ -162,13 +136,9 @@ public class MediatorServer : IMediatorServer
         byte[] data = new byte[1024];
         // send the notification to the node
         var notificationJson = JsonSerializer.Serialize(notification);
-
         var notificationBytes = Encoding.UTF8.GetBytes(notificationJson);
-
         var endPoint = new IPEndPoint(nodeNotificationHandlerType.IpAddress, nodeNotificationHandlerType.Port);
-
         var result = _socket.SendToAsync(notificationBytes, SocketFlags.None, endPoint);
-
         return Task.CompletedTask;
     }
 
@@ -177,23 +147,16 @@ public class MediatorServer : IMediatorServer
         byte[] data = new byte[1024];
         // send the request to the node
         var requestJson = JsonSerializer.Serialize(request);
-
         var requestBytes = Encoding.UTF8.GetBytes(requestJson);
-
         var endPoint = new IPEndPoint(nodeHandlerType.IpAddress, nodeHandlerType.Port);
-
         var result = _socket.SendToAsync(requestBytes, SocketFlags.None, endPoint);
 
         // wait for the response from the node
         var responseBytes = await _socket.ReceiveFromAsync(data, endPoint);
-
         var responseJson = Encoding.UTF8.GetString(data, 0, responseBytes.ReceivedBytes);
-
         var response = JsonSerializer.Deserialize(responseJson, Type.GetType(nodeHandlerType.ResponseType)!);
-
         return response;
     }
-
 
     private async Task ProcessMessagesAsync()
     {
